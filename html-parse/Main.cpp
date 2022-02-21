c658e980c63db79bbd8cce5eda5b48850a899c90
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2022-02-21 22:26:25 +0900

    fix

--- html-parse/Main.cpp
+++ html-parse/Main.cpp
@@ -12,18 +12,22 @@ int main(int argc, const char* argv[]) {
     // cout << "aaa" << endl;
     char html[] = "<div><span>HTML</span></div>";
 
+    // init.
     myhtml_t* myhtml = myhtml_create();
     myhtml_init(myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
-
     myhtml_tree_t* tree = myhtml_tree_create();
     myhtml_tree_init(tree, myhtml);
 
+    // parse.
     myhtml_parse(tree, MyENCODING_UTF_8, html, strlen(html));
 
+    // print.
     mycore_string_raw_t str = {0};
     myhtml_serialization_tree_buffer(myhtml_tree_get_document(tree), &str);
-    printf("%s\n", str.data);
+    cout << str.data << endl;
+    // printf("%s\n", str.data);
 
+    // release resource.
     mycore_string_raw_destroy(&str, false);
     myhtml_tree_destroy(tree);
     myhtml_destroy(myhtml);
