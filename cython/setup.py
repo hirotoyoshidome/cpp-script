134a3174fbcc3619f8fcd66506ea638b400c1d23
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2022-03-27 21:36:22 +0900

    fix

--- cython/setup.py
+++ cython/setup.py
@@ -4,16 +4,12 @@ from Cython.Build import cythonize
 
 sources = [
     "mycalc.pyx",
+    "sample.cpp",
 ]
 name = "mycalc"
-ext = Extension(name, sources=sources, include_dirs=['.'])
+ext = Extension(name, sources=sources, include_dirs=["."])
 
 setup(
     name=name,
-    ext_modules=cythonize(
-        [ext],
-        compiler_directives={
-            "language_level": "3"
-        }
-    ),
+    ext_modules=cythonize([ext], compiler_directives={"language_level": "3"}),
 )
