e886a59e117cb1814e58c93ebeeb17b75712013b
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2023-07-08 22:03:14 +0900

    add

---
+++ sample-c3/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+  printf("HELLO\n");
+  printf("HELL\n");
+  printf("H2ELL\n");
+  printf("aa");
+
+  bool aaa = true;
+  char abc[16] = "abc";
+  if (aaa)
+  {
+    printf("%s\n", abc);
+  }
+  
+
+  return 0;
+}
+
