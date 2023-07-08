08904f801668d433adf1bda253ea53c4342557c3
Hiroto Yoshidome <kichiru.3161026@gmail.com>
2023-07-08 22:24:18 +0900

    fix

--- sample-c3/main.c
+++ sample-c3/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
+int sumary(int *ary, int arylen)
+{
+  int _sum = 0;
+  for (int i = 0; i < arylen; i++)
+  {
+    _sum = _sum + ary[i];
+    // printf("%d\n", ary[i]);
+  }
+
+  return _sum;
+}
 
 int main()
 {
   printf("HELLO\n");
   printf("HELL\n");
   printf("H2ELL\n");
-  printf("aa");
+  printf("aa\n");
 
   bool aaa = true;
   char abc[16] = "abc";
@@ -14,7 +27,18 @@ int main()
   {
     printf("%s\n", abc);
   }
+
+  char *abc2 = &abc;
+  printf("%s\n", abc2);
+  // printf("%s\n", &abc2);
+  // printf("%s\n", *abc2);
+  // printf("%s\n", &abc);
   
+
+  int ary[5] = {1,1,2,2,3};
+  int arylen = 5;
+  int res = sumary(&ary, arylen);
+  printf("%d\n", res);
 
   return 0;
 }
